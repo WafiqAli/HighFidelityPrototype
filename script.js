e4b447c409b2cf863f1576dce8e5be56c8c72593
WafiqAli <wafiqcali@gmail.com>
2022-02-04 22:07:01 +1300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+/***************************** 
+
+Name: Wafiq Ali 
+Context: COMPSCI 345 Assignment 2, 2021
+My Hex Colour is #231A24
+
+*****************************/
+
+
+function openRegistrationModal() {
+    var modal = document.getElementById("RegisterModal");
+    modal.style.display = "block";
+}
+
+function closeRegistrationModal() {
+    var modal = document.getElementById("RegisterModal");
+    var closeSpan = document.getElementById("close");
+    
+    modal.style.display = "none";
+}
